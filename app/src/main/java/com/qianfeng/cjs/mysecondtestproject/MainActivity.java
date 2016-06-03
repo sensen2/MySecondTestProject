2dfd5eb6f8c220af99f39add34f439b142d8c783
sensen2 <1464353651@qq.com>
2016-06-03 17:36:13 +0800

    第二个项目第一次修改代码

--- app/src/main/java/com/qianfeng/cjs/mysecondtestproject/MainActivity.java
+++ app/src/main/java/com/qianfeng/cjs/mysecondtestproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.qianfeng.cjs.mysecondtestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.tv);
+        textView.setText("这是第二个项目第一次修改代码");
     }
 }
